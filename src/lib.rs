0c92022ea8e4418131b9bb003cf467a8bb2324ef
True Stack Mike <admin@truestack.dev>
2020-11-05 12:22:39 -0500

    added fetch post logic and created UI

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,147 @@
-// (Lines like the one below ignore selected Clippy rules
-//  - it's useful when you want to check your code with `cargo make verify`
-// but some rules are too "annoying" or are not applicable for your case.)
 #![allow(clippy::wildcard_imports)]
-
 use seed::{prelude::*, *};
 
-// ------ ------
-//     Init
-// ------ ------
-
 // `init` describes what should happen when your app started.
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+fn init(_: Url, orders: &mut impl Orders<Msg>) -> Model {
+    //initialize the model
+    let model = Model::default();
+
+    //make sure posts are loaded on init
+    orders.perform_cmd(async {
+        let response = fetch("https://jsonplaceholder.typicode.com/posts")
+            .await
+            .expect("HTTP request failed");
+
+        let posts: Vec<Post> = response
+            .check_status() // ensure we've got 2xx status
+            .expect("status check failed")
+            .json::<Vec<Post>>()
+            .await
+            .expect("deserialization failed");
+
+        Msg::PostsReceived(posts)
+    });
+    model
 }
 
-// ------ ------
-//     Model
-// ------ ------
-
 // `Model` describes our app state.
-type Model = i32;
+#[derive(Default)]
+struct Model {
+    posts: Vec<Post>,
+}
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+struct Post {
+    id: i32,
+    userId: i32,
+    title: String,
+    body: String,
+}
 
-// ------ ------
-//    Update
-// ------ ------
-
-// (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
-#[derive(Copy, Clone)]
 // `Msg` describes the different events you can modify state with.
 enum Msg {
-    Increment,
+    PostsReceived(Vec<Post>),
+    ViewPost(i32),
+    DeletePost(i32),
 }
 
 // `update` describes how to handle each `Msg`.
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
+    log!("update !!");
     match msg {
-        Msg::Increment => *model += 1,
+        Msg::ViewPost(_id) => {}
+        Msg::DeletePost(_id) => {}
+        Msg::PostsReceived(posts) => {
+            model.posts = posts;
+        }
     }
 }
 
-// ------ ------
-//     View
-// ------ ------
-
-// (Remove the line below once your `Model` become more complex.)
 #[allow(clippy::trivially_copy_pass_by_ref)]
 // `view` describes what to display.
 fn view(model: &Model) -> Node<Msg> {
+    log!("view!!");
     div![
-        "This is a counter: ",
-        C!["counter"],
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
+        C!["App"],
+        div![
+            C!["container", "home"],
+            h4![C!["center"], "Home",],
+            view_nav_bar(),
+            IF!(not(model.posts.is_empty()) => vec![
+                view_main(model),
+            ]),
+            IF!(model.posts.is_empty() => vec![
+                div![C!["center"],"No posts to show"]
+            ])
+        ]
     ]
 }
 
-// ------ ------
-//     Start
-// ------ ------
+fn view_main(model: &Model) -> Node<Msg> {
+    log!("view_main");
+    let posts = model.posts.iter();
+    div![posts.map(|post| {
+        div![
+            C!["post", "card"],
+            img![attrs![
+                At::Src => "assets/pokeball.png",
+                At::Alt => "A Pokeball"
+            ]],
+            div![
+                C!["card-content"],
+                a![
+                    attrs![
+                        At::Href => format!("/posts/{}",&post.id),
+                    ],
+                    span![C!["card-title", "red-text"], &post.title],
+                ],
+                p![&post.body]
+            ],
+        ]
+    }),]
+}
+
+fn view_nav_bar() -> Node<Msg> {
+    log!("view_nav_bar");
+    nav![
+        C!["nav-wrapper", "red", "darken-3"],
+        div![
+            C!["container"],
+            a![
+                C!["brand-logo"],
+                attrs![
+                    At::Href => "/",
+                ],
+                "Poke' Times"
+            ],
+            ul![
+                C!["right"],
+                li![a![
+                    attrs![
+                        At::Href => "/",
+                    ],
+                    "Home"
+                ],],
+                li![a![
+                    attrs![
+                        At::Href => "/about",
+                    ],
+                    "About"
+                ],],
+                li![a![
+                    attrs![
+                        At::Href => "/contact",
+                    ],
+                    "Contact"
+                ],]
+            ]
+        ]
+    ]
+}
 
 // (This function is invoked by `init` function in `index.html`.)
 #[wasm_bindgen(start)]
 pub fn start() {
+    log!("start ");
     // Mount the `app` to the element with the `id` "app".
-    App::start("app", init, update, view);
+    App::start("root", init, update, view);
 }
